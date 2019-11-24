053e6f751f98cfceed426c2a0616b0ede611345a
Patricia Milou <patriciamilou97@gmail.com>
2019-11-24 15:43:43 +0000

    added ultrasound

--- thesis/touch.py
+++ thesis/touch.py
@@ -7,14 +7,32 @@ import threading
 
 touch = 2
 motor = 3
+distance_echo = 14
+distance_trigger = 15
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(touch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(motor,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
+GPIO.setup(distance_echo,GPIO.IN)
+GPIO.setup(distance_trigger,GPIO.OUT)
 p = GPIO.PWM(motor, 50)     # Sets up pin 11 as a PWM pin
 p.start(0)                  # Starts running PWM on the pin and sets it to 0
 
 overall_state = {"sensor_moved": False, "last_moved": time.time(), "touchstatus":False, "alternate": False, "motor": p}
+
+def distance():
+    GPIO.output(distance_trigger,True)
+    time.sleep(0.0000001)
+    GPIO.output(distance_trigger,False)
+    start_time = time.time()
+    stop_time = time.time()
+    while GPIO.input(distance_echo) == 0:
+        start_time = time.time()
+    while GPIO.input(distance_echo) == 1:
+        stop_time = time.time()
+    time_elapsed = stop_time - start_time
+    distance = (time_elapsed * 34300)/2
+    return distance
 
 def setAngle(angle):
     global overall_state
@@ -63,9 +81,11 @@ def read_touchsensor():
         time.sleep(0.02)
 
 def main():
-    automatic_thread = threading.Thread(target=automatic)
-    automatic_thread.start()
-    read_touchsensor()
+    while True:
+        print(distance())
+    # automatic_thread = threading.Thread(target=automatic)
+    # automatic_thread.start()
+    # read_touchsensor()
     # touchsensor_thread = threading.Thread(target=read_touchsensor)
     # touchsensor_thread.start()
 
